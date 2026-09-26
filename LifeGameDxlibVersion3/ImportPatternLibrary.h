04e43a13a13902111a58a9c724f29a4640e37655
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-26 18:14:31 +0900

    Add imported pattern rename and delete API

--- LifeGameDxlibVersion3/ImportPatternLibrary.h
+++ LifeGameDxlibVersion3/ImportPatternLibrary.h
@@ -8,6 +8,7 @@
 
 struct ImportPattern {
     std::string name;
+    std::string fileName;
     int width = 0;
     int height = 0;
     std::vector<PatternCell> cells;
@@ -18,6 +19,9 @@ public:
     bool load(std::string& errorMessage);
     bool importFile(const std::string& sourcePath, std::size_t& importedIndex,
                     std::string& errorMessage);
+    bool rename(std::size_t index, const std::string& newName,
+                std::size_t& renamedIndex, std::string& errorMessage);
+    bool remove(std::size_t index, std::string& errorMessage);
 
     std::size_t size() const noexcept { return patterns_.size(); }
     const ImportPattern& at(std::size_t index) const { return patterns_.at(index); }
